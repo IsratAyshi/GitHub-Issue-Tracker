26ec80c47d83b1bf1e8cfa75033e293a07dd00d0
Israt Jahan Ayshi <israt.ayshi1999@gmail.com>
2026-03-07 05:16:17 +0600

    login page designed, loging in  with username & password and connect to home page succesfully.

--- scripts/login.js
+++ scripts/login.js
@@ -1,2 +1,14 @@
+console.log("login functionality initiated");
 
-console.log('login functionality initiated')
+document.getElementById("signIn-btn").addEventListener("click", () => {
+  const username = document.getElementById("input-username").value;
+  const password = document.getElementById("input-password").value;
+
+  if (username === "admin" && password === "admin123") {
+    alert("Login successful");
+
+    window.location.assign("./home.html");
+  } else {
+    alert("Invalid username or password");
+  }
+});
